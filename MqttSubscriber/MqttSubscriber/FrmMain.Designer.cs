609b33bf0e24519579c3f98e8c7e37973b4e291b
personar95 <personar95@gmail.com>
2020-04-04 14:49:37 +0900

    최초커밋

---
+++ MqttSubscriber/MqttSubscriber/FrmMain.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace MqttSubscriber
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtConnectionString = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtClientId = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtTopic = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtPayload = new System.Windows.Forms.TextBox();
+            this.BtnConnect = new System.Windows.Forms.Button();
+            this.BtnDisconnect = new System.Windows.Forms.Button();
+            this.RtbRecieved = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(22, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connection String";
+            // 
+            // TxtConnectionString
+            // 
+            this.TxtConnectionString.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TxtConnectionString.Location = new System.Drawing.Point(179, 16);
+            this.TxtConnectionString.Name = "TxtConnectionString";
+            this.TxtConnectionString.Size = new System.Drawing.Size(391, 25);
+            this.TxtConnectionString.TabIndex = 1;
+            this.TxtConnectionString.Text = "192.168.0.5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(22, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Client ID";
+            // 
+            // TxtClientId
+            // 
+            this.TxtClientId.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TxtClientId.Location = new System.Drawing.Point(179, 47);
+            this.TxtClientId.Name = "TxtClientId";
+            this.TxtClientId.Size = new System.Drawing.Size(391, 25);
+            this.TxtClientId.TabIndex = 1;
+            this.TxtClientId.Text = "HugoSubscriber";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(22, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 17);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Topic";
+            // 
+            // TxtTopic
+            // 
+            this.TxtTopic.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TxtTopic.Location = new System.Drawing.Point(179, 78);
+            this.TxtTopic.Name = "TxtTopic";
+            this.TxtTopic.Size = new System.Drawing.Size(391, 25);
+            this.TxtTopic.TabIndex = 1;
+            this.TxtTopic.Text = "home/sensor1/data/";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(22, 112);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Payload";
+            // 
+            // TxtPayload
+            // 
+            this.TxtPayload.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TxtPayload.Location = new System.Drawing.Point(179, 109);
+            this.TxtPayload.Name = "TxtPayload";
+            this.TxtPayload.Size = new System.Drawing.Size(391, 25);
+            this.TxtPayload.TabIndex = 1;
+            // 
+            // BtnConnect
+            // 
+            this.BtnConnect.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnConnect.Location = new System.Drawing.Point(695, 97);
+            this.BtnConnect.Name = "BtnConnect";
+            this.BtnConnect.Size = new System.Drawing.Size(113, 37);
+            this.BtnConnect.TabIndex = 2;
+            this.BtnConnect.Text = "Connect";
+            this.BtnConnect.UseVisualStyleBackColor = true;
+            this.BtnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // BtnDisconnect
+            // 
+            this.BtnDisconnect.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnDisconnect.Location = new System.Drawing.Point(814, 97);
+            this.BtnDisconnect.Name = "BtnDisconnect";
+            this.BtnDisconnect.Size = new System.Drawing.Size(113, 37);
+            this.BtnDisconnect.TabIndex = 2;
+            this.BtnDisconnect.Text = "Disconnect";
+            this.BtnDisconnect.UseVisualStyleBackColor = true;
+            this.BtnDisconnect.Click += new System.EventHandler(this.BtnDisconnect_Click);
+            // 
+            // RtbRecieved
+            // 
+            this.RtbRecieved.Enabled = false;
+            this.RtbRecieved.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.RtbRecieved.Location = new System.Drawing.Point(12, 145);
+            this.RtbRecieved.Name = "RtbRecieved";
+            this.RtbRecieved.Size = new System.Drawing.Size(920, 441);
+            this.RtbRecieved.TabIndex = 3;
+            this.RtbRecieved.Text = "";
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(944, 598);
+            this.Controls.Add(this.RtbRecieved);
+            this.Controls.Add(this.BtnDisconnect);
+            this.Controls.Add(this.BtnConnect);
+            this.Controls.Add(this.TxtPayload);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtTopic);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtClientId);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtConnectionString);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmMain";
+            this.Text = "MQTT Client";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtConnectionString;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtClientId;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtTopic;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtPayload;
+        private System.Windows.Forms.Button BtnConnect;
+        private System.Windows.Forms.Button BtnDisconnect;
+        private System.Windows.Forms.RichTextBox RtbRecieved;
+    }
+}
+
---
+++ MqttSubscriber/MqttSubscriber/FrmMain.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MqttSubscriber
+{
+    public partial class FrmMain : Form
+    {
+        MqttClient client;
+        delegate void UpdateTextCallback(string message);
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                IPAddress hostIP;
+                hostIP = IPAddress.Parse(TxtConnectionString.Text);
+                client = new MqttClient(hostIP);
+
+                client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void Client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            try
+            {
+                //UpdateText(">>> Received Message");
+                //UpdateText(">>> Topic: " + e.Topic);
+                UpdateText(">>> Message: " + Encoding.UTF8.GetString(e.Message));
+
+                // 메시지가 발생할 경우 DB에 저장
+            }
+            catch (Exception ex)
+            {
+                UpdateText("[ERROR] " + ex.Message);
+            }
+        }
+
+        private void UpdateText(string message)
+        {
+            if (RtbRecieved.InvokeRequired)
+            {
+                UpdateTextCallback d = new UpdateTextCallback(UpdateText);
+                this.Invoke(d, new object[] { message });
+            }
+            else
+            {
+                this.RtbRecieved.AppendText(message + "\n");
+                this.RtbRecieved.ScrollToCaret();
+            }
+        }
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client.Connect(TxtClientId.Text + "_sub");
+                UpdateText(">>>>> Client connected");
+                client.Subscribe(new string[] { TxtTopic.Text }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+                UpdateText(">>>>> Subscribing to : " + TxtTopic.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void BtnDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client.Disconnect();
+                UpdateText(">> Client disconnected");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
