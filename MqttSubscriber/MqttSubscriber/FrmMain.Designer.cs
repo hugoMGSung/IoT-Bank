acd4e6571305544935a265c6849423af6a19087a
HugoMGSung <personar95@naver.com>
2020-06-27 12:16:57 +0900

    소스 수정(서보모터 부재)

--- MqttSubscriber/MqttSubscriber/FrmMain.Designer.cs
+++ MqttSubscriber/MqttSubscriber/FrmMain.Designer.cs
@@ -35,97 +35,76 @@
             this.TxtClientId = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.TxtTopic = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.TxtPayload = new System.Windows.Forms.TextBox();
             this.BtnConnect = new System.Windows.Forms.Button();
             this.BtnDisconnect = new System.Windows.Forms.Button();
             this.RtbRecieved = new System.Windows.Forms.RichTextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.TxtControlTemp = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtPayload = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(22, 19);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(22, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(131, 17);
+            this.label1.Size = new System.Drawing.Size(127, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Connection String";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // TxtConnectionString
             // 
-            this.TxtConnectionString.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.TxtConnectionString.Location = new System.Drawing.Point(179, 16);
+            this.TxtConnectionString.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TxtConnectionString.Location = new System.Drawing.Point(155, 15);
             this.TxtConnectionString.Name = "TxtConnectionString";
-            this.TxtConnectionString.Size = new System.Drawing.Size(391, 25);
+            this.TxtConnectionString.Size = new System.Drawing.Size(391, 22);
             this.TxtConnectionString.TabIndex = 1;
-            this.TxtConnectionString.Text = "192.168.0.5";
+            this.TxtConnectionString.Text = "127.0.0.1";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label2.Location = new System.Drawing.Point(22, 50);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(22, 43);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(67, 17);
+            this.label2.Size = new System.Drawing.Size(127, 20);
             this.label2.TabIndex = 0;
             this.label2.Text = "Client ID";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // TxtClientId
             // 
-            this.TxtClientId.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.TxtClientId.Location = new System.Drawing.Point(179, 47);
+            this.TxtClientId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TxtClientId.Location = new System.Drawing.Point(155, 42);
             this.TxtClientId.Name = "TxtClientId";
-            this.TxtClientId.Size = new System.Drawing.Size(391, 25);
+            this.TxtClientId.Size = new System.Drawing.Size(391, 22);
             this.TxtClientId.TabIndex = 1;
             this.TxtClientId.Text = "HugoSubscriber";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label3.Location = new System.Drawing.Point(22, 81);
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(22, 70);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(114, 17);
+            this.label3.Size = new System.Drawing.Size(127, 20);
             this.label3.TabIndex = 0;
             this.label3.Text = "Subscribe Topic";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // TxtTopic
             // 
-            this.TxtTopic.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.TxtTopic.Location = new System.Drawing.Point(179, 78);
+            this.TxtTopic.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TxtTopic.Location = new System.Drawing.Point(155, 69);
             this.TxtTopic.Name = "TxtTopic";
-            this.TxtTopic.Size = new System.Drawing.Size(391, 25);
+            this.TxtTopic.Size = new System.Drawing.Size(391, 22);
             this.TxtTopic.TabIndex = 1;
             this.TxtTopic.Text = "home/device/data/";
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label4.Location = new System.Drawing.Point(22, 112);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(99, 17);
-            this.label4.TabIndex = 0;
-            this.label4.Text = "Publish Topic";
-            // 
-            // TxtPayload
-            // 
-            this.TxtPayload.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.TxtPayload.Location = new System.Drawing.Point(179, 109);
-            this.TxtPayload.Name = "TxtPayload";
-            this.TxtPayload.Size = new System.Drawing.Size(391, 25);
-            this.TxtPayload.TabIndex = 1;
-            this.TxtPayload.Text = "home/device/control/";
-            // 
             // BtnConnect
             // 
-            this.BtnConnect.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.BtnConnect.Location = new System.Drawing.Point(700, 128);
+            this.BtnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnConnect.Location = new System.Drawing.Point(700, 84);
             this.BtnConnect.Name = "BtnConnect";
-            this.BtnConnect.Size = new System.Drawing.Size(113, 37);
+            this.BtnConnect.Size = new System.Drawing.Size(113, 32);
             this.BtnConnect.TabIndex = 2;
             this.BtnConnect.Text = "Connect";
             this.BtnConnect.UseVisualStyleBackColor = true;
@@ -133,10 +112,10 @@
             // 
             // BtnDisconnect
             // 
-            this.BtnDisconnect.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.BtnDisconnect.Location = new System.Drawing.Point(819, 128);
+            this.BtnDisconnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.BtnDisconnect.Location = new System.Drawing.Point(819, 84);
             this.BtnDisconnect.Name = "BtnDisconnect";
-            this.BtnDisconnect.Size = new System.Drawing.Size(113, 37);
+            this.BtnDisconnect.Size = new System.Drawing.Size(113, 32);
             this.BtnDisconnect.TabIndex = 2;
             this.BtnDisconnect.Text = "Disconnect";
             this.BtnDisconnect.UseVisualStyleBackColor = true;
@@ -145,42 +124,39 @@
             // RtbRecieved
             // 
             this.RtbRecieved.Enabled = false;
-            this.RtbRecieved.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.RtbRecieved.Location = new System.Drawing.Point(12, 171);
+            this.RtbRecieved.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.RtbRecieved.Location = new System.Drawing.Point(12, 123);
             this.RtbRecieved.Name = "RtbRecieved";
-            this.RtbRecieved.Size = new System.Drawing.Size(920, 415);
+            this.RtbRecieved.Size = new System.Drawing.Size(920, 385);
             this.RtbRecieved.TabIndex = 3;
             this.RtbRecieved.Text = "";
             // 
-            // label5
+            // label4
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label5.Location = new System.Drawing.Point(22, 143);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(150, 17);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "Control Temperature";
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(22, 97);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(127, 19);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Payload";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
-            // TxtControlTemp
+            // TxtPayload
             // 
-            this.TxtControlTemp.Font = new System.Drawing.Font("나눔고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.TxtControlTemp.Location = new System.Drawing.Point(179, 140);
-            this.TxtControlTemp.Name = "TxtControlTemp";
-            this.TxtControlTemp.Size = new System.Drawing.Size(391, 25);
-            this.TxtControlTemp.TabIndex = 1;
-            this.TxtControlTemp.Text = "25.0, 23.0";
+            this.TxtPayload.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.TxtPayload.Location = new System.Drawing.Point(155, 95);
+            this.TxtPayload.Name = "TxtPayload";
+            this.TxtPayload.Size = new System.Drawing.Size(391, 22);
+            this.TxtPayload.TabIndex = 1;
             // 
             // FrmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(944, 598);
+            this.ClientSize = new System.Drawing.Size(944, 518);
             this.Controls.Add(this.RtbRecieved);
             this.Controls.Add(this.BtnDisconnect);
             this.Controls.Add(this.BtnConnect);
-            this.Controls.Add(this.TxtControlTemp);
-            this.Controls.Add(this.label5);
             this.Controls.Add(this.TxtPayload);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.TxtTopic);
@@ -208,13 +184,11 @@
         private System.Windows.Forms.TextBox TxtClientId;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox TxtTopic;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox TxtPayload;
         private System.Windows.Forms.Button BtnConnect;
         private System.Windows.Forms.Button BtnDisconnect;
         private System.Windows.Forms.RichTextBox RtbRecieved;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.TextBox TxtControlTemp;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtPayload;
     }
 }
 
--- MqttSubscriber/MqttSubscriber/FrmMain.cs
+++ MqttSubscriber/MqttSubscriber/FrmMain.cs
@@ -1,9 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Net;
 using System.Text;
 using System.Windows.Forms;
@@ -17,33 +15,21 @@ namespace MqttSubscriber
         MqttClient client;
         private string connectionString;
         private ulong line_count;
-        private float open_temp, close_temp;
-        private bool opened;
-
         delegate void UpdateTextCallback(string message);
 
         public FrmMain()
         {
             InitializeComponent();
-
             InitAllData();
         }
 
         private void InitAllData()
         {
-            connectionString = "Server=localhost;Port=3306;Database=iot_data;Uid=root;Pwd=mysql_p@ssw0rd";
-
+            connectionString = "Server=localhost;Port=3306;"+
+                                "Database=iot_data;Uid=root;Pwd=maria_p@ssw0rd!";
             line_count = 0;
-
             BtnConnect.Enabled = true;
             BtnDisconnect.Enabled = false;
-
-
-            var temps = TxtControlTemp.Text.Split(',');
-            opened = false; // 모터 컨트롤로 오픈했는지? 
-            open_temp = float.Parse(temps[0].Trim());
-            close_temp = float.Parse(temps[1].Trim());
-            UpdateText($"Initialized - Open Temperature {open_temp}°C / Close Temperature {close_temp}°C");
         }
 
         private void FrmMain_Load(object sender, EventArgs e)
@@ -53,7 +39,6 @@ namespace MqttSubscriber
                 IPAddress hostIP;
                 hostIP = IPAddress.Parse(TxtConnectionString.Text);
                 client = new MqttClient(hostIP);
-
                 client.MqttMsgPublishReceived += Client_MqttMsgPublishReceived;
             }
             catch (Exception ex)
@@ -67,84 +52,32 @@ namespace MqttSubscriber
             try
             {
                 var message = Encoding.UTF8.GetString(e.Message);
-                //UpdateText(">>> Received Message");
-                //UpdateText(">>> Topic: " + e.Topic);
                 UpdateText(">>> Message: " + message);
-
-                // 메시지가 발생할 경우 DB에 저장
-                InsertData(message);
-
-                // 만약 알람이 발생하면 데이터 디바이스로 재전송
-                // To do...
-                SendToBroker(message);
+                InsertData(message); // 메시지가 발생할 경우 DB에 저장
             }
             catch (Exception ex)
             {
                 UpdateText("[ERROR] " + ex.Message);
             }
         }
-
-        private void SendToBroker(string message)
-        {
-            Dictionary<string, string> currentDatas = JsonConvert.DeserializeObject<Dictionary<string, string>>(message);
-
-            var uuid = currentDatas["uuid"];
-            var currTemp = float.Parse(currentDatas["temperature"]);
-            Debug.WriteLine(currTemp);
-
-            JObject json = new JObject();
-            if (currTemp >= open_temp)
-            {
-                if (opened == false)
-                {
-                    json.Add("uuid", uuid);
-                    json.Add("degree", 12.5);
-                    string strJson = JsonConvert.SerializeObject(json);
-                    client.Publish(TxtPayload.Text, Encoding.Default.GetBytes(strJson), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
-                    Debug.WriteLine(json);
-
-                    opened = true;
-                    UpdateText("Server Motor open");
-                }                
-                
-            }
-            else if (currTemp <= close_temp)
-            {
-                if (opened)
-                {
-                    json.Add("uuid", uuid);
-                    json.Add("degree", 2.5);
-                    string strJson = JsonConvert.SerializeObject(json);
-                    client.Publish(TxtPayload.Text, Encoding.Default.GetBytes(strJson), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
-                    Debug.WriteLine(json);
-
-                    opened = false;
-                    UpdateText("Server Motor close");
-                }
-                
-            }
-        }
-
         private void InsertData(string message)
         {
-            Dictionary<string, string> currentDatas = JsonConvert.DeserializeObject<Dictionary<string, string>>(message);
-            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            var currentDatas = JsonConvert.DeserializeObject<Dictionary<string, string>>(message);
+            using (var conn = new MySqlConnection(connectionString))
             {
-                string strInsertQry = string.Format("INSERT INTO iot_data.sensordata (idx, uuid, gentime, temperature, humidity, systems) " +
-                                                    "VALUES(NULL, '{0}', '{1}', {2}, {3}, 'SYSTEM'); ", 
-                                                    currentDatas["uuid"], currentDatas["time"], 
-                                                    currentDatas["temperature"], currentDatas["humidity"]);
+                string strInsertQry = string.Format("INSERT INTO iot_data.sensordata " +
+                                        " (idx, dev_id, time, temp, humid) " +
+                                        "VALUES (NULL, '{0}', '{1}', {2}, {3}); ", 
+                                        currentDatas["dev_id"], currentDatas["time"], 
+                                        currentDatas["temp"], currentDatas["humid"]);
                 try
                 {
                     conn.Open();
                     MySqlCommand cmd = new MySqlCommand(strInsertQry, conn);
                     if (cmd.ExecuteNonQuery() == 1)
-                    {
                         UpdateText("[DB] " + "Insert succeed");
-                    } else
-                    {
+                    else
                         UpdateText("[DB] " + "Insert failed");
-                    }
                 }
                 catch (Exception ex)
                 {
@@ -170,43 +103,23 @@ namespace MqttSubscriber
 
         private void BtnConnect_Click(object sender, EventArgs e)
         {
-            try
-            {
-                client.Connect(TxtClientId.Text + "_sub");
-                UpdateText(">>>>> Client connected");
-                client.Subscribe(new string[] { TxtTopic.Text }, new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
-                UpdateText(">>>>> Subscribing to : " + TxtTopic.Text);
+            client.Connect(TxtClientId.Text + "_sub");
+            UpdateText(">>>>> Client connected");
+            client.Subscribe(new string[] { TxtTopic.Text }, 
+                new byte[] { MqttMsgBase.QOS_LEVEL_AT_LEAST_ONCE });
+            UpdateText(">>>>> Subscribing to : " + TxtTopic.Text);
 
-                BtnConnect.Enabled = false;
-                BtnDisconnect.Enabled = true;
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-
-                BtnConnect.Enabled = true;
-                BtnDisconnect.Enabled = false;
-            }
+            BtnConnect.Enabled = false;
+            BtnDisconnect.Enabled = true;
         }
 
         private void BtnDisconnect_Click(object sender, EventArgs e)
         {
-            try
-            {
-                client.Disconnect();
-                UpdateText(">> Client disconnected");
+            client.Disconnect();
+            UpdateText(">> Client disconnected");
 
-                BtnConnect.Enabled = true;
-                BtnDisconnect.Enabled = false;
-
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString());
-
-                BtnConnect.Enabled = false;
-                BtnDisconnect.Enabled = true;
-            }
+            BtnConnect.Enabled = true;
+            BtnDisconnect.Enabled = false;
         }
     }
 }
